e11b2d4c1eecb956137cc8f0574013b54b23511b
takenoko <takenoko@mail.nokotaro.com>
2023-06-24 16:25:51 +0900

    Update index.ts

--- packages/pds/src/db/migrations/index.ts
+++ packages/pds/src/db/migrations/index.ts
@@ -1,3 +1,5 @@
 // NOTE this file can be edited by hand, but it is also appended to by the migration:create command.
 // It's important that every migration is exported from here with the proper name. We'd simplify
 // this with kysely's FileMigrationProvider, but it doesn't play nicely with the build process.
+
+export * as _20230530T213530067Z from './20230530T213530067Z-rebase-indices'
