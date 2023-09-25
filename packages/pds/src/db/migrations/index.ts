439603de697761ce6f0822082e3dbf8b9f95d9c8
takenoko <takenoko@mail.nokotaro.com>
2023-09-25 13:39:12 +0900

    wip

--- packages/pds/src/db/migrations/index.ts
+++ packages/pds/src/db/migrations/index.ts
@@ -2,6 +2,8 @@
 // It's important that every migration is exported from here with the proper name. We'd simplify
 // this with kysely's FileMigrationProvider, but it doesn't play nicely with the build process.
 
+export * as _20230529T222706121Z from './20230529T222706121Z-suggested-follows'
+export * as _20230530T213530067Z from './20230530T213530067Z-rebase-indices'
 export * as _20230605T235529700Z from './20230605T235529700Z-outgoing-repo-seq'
 export * as _20230703T044601833Z from './20230703T044601833Z-feed-and-label-indices'
 export * as _20230718T170914772Z from './20230718T170914772Z-sequencer-leader-sequence'
